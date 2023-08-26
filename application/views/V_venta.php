b4d45998f8b7b2510dd7ee0142ac01405592cf2d
Christony560 <espanachristian560@gmail.com>
2023-08-26 12:19:24 -0600

    cambio venta segun modelo ER

--- application/views/V_venta.php
+++ application/views/V_venta.php
@@ -21,23 +21,16 @@
                         <option value="<?php echo $cliente->cliente_id; ?>"><?php echo $cliente->cliente_nombre; ?> <?php echo $cliente->cliente_apellido; ?></option>
                     <?php endforeach; ?>
                 </select>
-            <div class="form-group mt-2 ">
-                <label for="producto">Producto</label>
-                <input type="text" class="form-style" name="producto" id="producto" required>
-            </div>
-            <div class="form-group">
-                <label for="cantidad">Cantidad</label>
-                <input type="number" class="form-style" name="cantidad" id="cantidad" required>
-            </div>
-            <div class="form-group">
-                <label for="precio">Precio</label>
-                <input type="number" class="form-style" name="precio" id= "precio" required>
-            </div>
-            <div class="form-group">
-                <label for="total">Total</label>
-                <input type="number" name="total" id ="total"class="form-style" required>
-            </div>
-            <button type="submit" class="btn" name="Guardar">Guardar</button>
+            <label>Metodo Pago</label>
+            <select name="metodo_pago" id="metodo_pago" class="form-style" required>
+                <option value="">Seleccione un metodo de pago</option>
+                <option value="1">Tarjeta de Crédito</option>
+                <option value="2">Transferencia Bancaria</option>
+                <option value="3">Efectivo</option>
+                <option value="4">Cheque</option>
+                <option value="5">Pago Móvil</option>
+                </select>
+             <button type="submit" class="btn" name="Guardar">Guardar</button>
             <button class="btn" onclick="location.href='<?php echo site_url('dashboard/index'); ?>'">Regresar</button>
  
         </form>
