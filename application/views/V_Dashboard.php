2c7e2a890b00bf8bb067a41c1dc0310b0998c938
Christony560 <espanachristian560@gmail.com>
2023-08-26 20:41:04 -0600

    Eliminacion de botones extra en Dashboard

--- application/views/V_Dashboard.php
+++ application/views/V_Dashboard.php
@@ -16,11 +16,6 @@
 
 	<button class ="mb-2"onclick="location.href='<?php echo site_url('usuario/index'); ?>'">Modificación / Eliminación de Usuarios Registrados</button> <!--redireccion al sitio-->
 	<button class ="mb-2" onclick="location.href='<?php echo site_url('venta/index');?>'">Iniciar Proceso Venta</button>
-	<button class ="mb-2" onclick="location.href='<?php echo site_url('producto/index');?>'">Ingreso Productos</button>
-	<button onclick="location.href='http://localhost/proyecto/Tienda/1.%20Alquiler.php'">Ingreso Alquiler</button>
-	<button onclick="location.href='http://localhost/proyecto/Tienda/5.%20Factura.html'">Registro de Facturas</button>
-	<button onclick="location.href='http://localhost/proyecto/Tienda/7.DetalleProducto.html'">Detalle Facturas Productos</button>
-	<button onclick="location.href='http://localhost/proyecto/Tienda/6%20DetalleAlquiler.php'">Detalle Facturas Alquileres</button>
-	
+	<button class ="mb-2" onclick="location.href='<?php echo site_url('producto/index');?>'">Ingreso de Productos</button>	
 </body>	
 </html>
