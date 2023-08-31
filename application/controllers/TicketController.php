a724ecb996985a553220aba76ad8bc37a8f3592c
Christony560 <espanachristian560@gmail.com>
2023-08-31 13:07:53 -0600

    Añadido de dompdf con Composer

--- application/controllers/TicketController.php
+++ application/controllers/TicketController.php
@@ -3,18 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class TicketController extends CI_Controller{
     public function __construct(){
         parent::__construct();
+        require_once(APPPATH.'libraries/dompdf/autoload.inc.php');
+        $this->dompdf = new \Dompdf\Dompdf();
         $this->load->model('TicketModel');
-        require_once(APPPATH.'libraries/fpdf/fpdf.php');
-      
     }
 
     public function index(){
         $data['detalles'] = $this->TicketModel->getDetalle();
         $this->load->view('V_ticket', $data);
     }
-    
-   
-    
 }
 
 
